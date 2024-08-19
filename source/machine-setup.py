a065a8228ddc2b53e32758961b9cb15cc713d3df
Jason Huggins <jrhuggins@gmail.com>
2024-08-19 11:30:03 -0500

    Fix for Valet Link and Comitup

--- source/machine-setup.py
+++ source/machine-setup.py
@@ -14,14 +14,17 @@ shell("sudo apt-get install -y python3.11-venv")
 # Raspberry Pi Config
 shell("sudo raspi-config nonint do_vnc 0") #Enable VNC
 
+##########################################
 # For Valet Vision Only
-shell("sudo raspi-config nonint do_spi 0") #Enable SPI
-shell("sudo raspi-config nonint do_i2c 0") # Enable I2c
+#shell("sudo raspi-config nonint do_spi 0") #Enable SPI
+#shell("sudo raspi-config nonint do_i2c 0") # Enable I2c
 # Install libcamera libraries
-shell("sudo apt-get install -y libcamera-v4l2 libcamera-tools libcamera-apps")
+#shell("sudo apt-get install -y libcamera-v4l2 libcamera-tools libcamera-apps")
+##########################################
+
 
 # Install OCR packages
-shell("sudo apt install tesseract-ocr")
+shell("sudo apt install -y tesseract-ocr")
 
 # Create Python Virtual Environment
 shell("cd /home/tapster/; mkdir -p Projects/valet")
@@ -49,21 +52,23 @@ shell("""cd /home/tapster/Projects/valet;
          source env/bin/activate;
          python3 -m pip install ./zero-hid/;""")
 
+##########################################
 # For Valet Vision Only
 # Install Adafruit libraries
-shell("""cd /home/tapster/Projects/valet;
-         source env/bin/activate;
-         python3 -m pip install --upgrade adafruit-python-shell;
-         python3 -m pip install adafruit-circuitpython-rgb-display;""")
-
-shell("""cd /home/tapster/Projects/valet;
-         source env/bin/activate;
-         wget https://raw.githubusercontent.com/adafruit/Raspberry-Pi-Installer-Scripts/master/raspi-blinka.py;
-         echo "n" | sudo -E env PATH=$PATH python3 raspi-blinka.py;""")
-
-# Install Tapster LCD display test script
-shell("""cd /home/tapster/Projects/valet;
-         git clone https://gist.github.com/hugs/559aa69d8870630bda790e77847f9847 setup-test;""")
+#shell("""cd /home/tapster/Projects/valet;
+#         source env/bin/activate;
+#         python3 -m pip install --upgrade adafruit-python-shell;
+#         python3 -m pip install adafruit-circuitpython-rgb-display;""")
+#
+#shell("""cd /home/tapster/Projects/valet;
+#         source env/bin/activate;
+#         wget https://raw.githubusercontent.com/adafruit/Raspberry-Pi-Installer-Scripts/master/raspi-blinka.py;
+#         echo "n" | sudo -E env PATH=$PATH python3 raspi-blinka.py;""")
+#
+## Install Tapster LCD display test script
+#shell("""cd /home/tapster/Projects/valet;
+#         git clone https://gist.github.com/hugs/559aa69d8870630bda790e77847f9847 setup-test;""")
+##########################################
 
 # Install Checkbox server
 shell("""cd /home/tapster/Projects/valet;
@@ -79,43 +84,46 @@ shell("""cd /home/tapster/Projects/valet;
          cd checkbox-client-python;
          python3 -m pip install -r requirements.txt;""")
 
+##########################################
 # For Valet Link Only:
 # Required video capture settings
-#shell("""echo "dtoverlay=tc358743" | sudo tee -a /boot/firmware/config.txt;
-#         echo "dtoverlay=tc358743-audio" | sudo tee -a /boot/firmware/config.txt;""")
+shell("""echo "dtoverlay=tc358743" | sudo tee -a /boot/firmware/config.txt;
+         echo "dtoverlay=tc358743-audio" | sudo tee -a /boot/firmware/config.txt;""")
 
 # For Valet Link Only:
 # Required video capture settings
-#shell("""sudo truncate -s-1 /boot/firmware/cmdline.txt;
-#         echo -n " cma=96M" | sudo tee -a /boot/firmware/cmdline.txt;""")
+shell("""sudo truncate -s-1 /boot/firmware/cmdline.txt;
+         echo -n " cma=96M" | sudo tee -a /boot/firmware/cmdline.txt;""")
 
 # For Valet Link Only:
-# Install Checkbox Server Service
-#shell("""cd /home/tapster/Projects/valet/checkbox-server/service;
-#         sudo cp checkbox-server-hdmi.service /etc/systemd/system/checkbox-server-hdmi.service;
-#         sudo chmod 644 /etc/systemd/system/checkbox-server-hdmi.service;
-#         sudo systemctl daemon-reload;
-#         sudo systemctl start checkbox-server-hdmi;
-#         sudo systemctl enable checkbox-server-hdmi;""")
-
-# For Valet Vision Only:
 # Install Checkbox Server Service
 shell("""cd /home/tapster/Projects/valet/checkbox-server/service;
-         sudo cp checkbox-server-camera.service /etc/systemd/system/checkbox-server-camera.service;
-         sudo chmod 644 /etc/systemd/system/checkbox-server-camera.service;
+         sudo cp checkbox-server-hdmi.service /etc/systemd/system/checkbox-server-hdmi.service;
+         sudo chmod 644 /etc/systemd/system/checkbox-server-hdmi.service;
          sudo systemctl daemon-reload;
-         sudo systemctl start checkbox-server-camera;
-         sudo systemctl enable checkbox-server-camera;""")
+         sudo systemctl start checkbox-server-hdmi;
+         sudo systemctl enable checkbox-server-hdmi;""")
+##########################################
 
+##########################################
+# For Valet Vision Only:
+# Install Checkbox Server Service
+#shell("""cd /home/tapster/Projects/valet/checkbox-server/service;
+#         sudo cp checkbox-server-camera.service /etc/systemd/system/checkbox-server-camera.service;
+#         sudo chmod 644 /etc/systemd/system/checkbox-server-camera.service;
+#         sudo systemctl daemon-reload;
+#         sudo systemctl start checkbox-server-camera;
+#         sudo systemctl enable checkbox-server-camera;""")
+##########################################
 
 # Install Comitup
 shell("""cd /home/tapster/Projects/valet/;
          mkdir comitup;
          cd comitup;
-         wget https://davesteele.github.io/comitup/latest/davesteele-comitup-apt-source_latest.deb;
-         sudo dpkg -i --force-all davesteele-comitup-apt-source_latest.deb;
+         wget https://davesteele.github.io/comitup/deb/comitup_1.42-1_all.deb;
+         sudo dpkg -i --force-all comitup_*.deb;
          sudo apt-get update;
-         sudo apt-get install -y comitup comitup-watch;""")
+         sudo apt-get install -y comitup;""")
 
 # Allow NetworkManager to manage the wifi interfaces
 shell("""mkdir -pv /home/tapster/.ciusafe/etc/network/interfaces;
